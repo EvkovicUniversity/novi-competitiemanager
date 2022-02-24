bbafe1dbda9afbe06c59d77eca9b1c7c5669c06a
Evan Olijrhook <e.olijrhook@novi-education.nl>
2022-02-24 13:49:01 +0100

    initial commit

---
+++ src/view/components/app-base/App.js
@@ -0,0 +1,17 @@
+import logo from './view/resources/logo/Novi_Logo_Schematisch.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <div className="background-image">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+
+          </header>
+        </div>
+    </div>
+  );
+}
+
+export default App;
