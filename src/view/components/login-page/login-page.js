d00ddffea6f29a74bf0b95725d17e16d5a606f00
unknown <e.olijrhook@noviuniversity.com>
2022-02-25 14:57:12 +0100

    ook login in klasse omgezet

--- src/view/components/login-page/login-page.js
+++ src/view/components/login-page/login-page.js
@@ -1,21 +1,24 @@
+import React from "react";
 import "../algemeen/algemeen.css";
 import "./login-page-css.css";
 
-function LoginPage() {
-    return (
-        <div>
-            <h1>Novi's competitiemanager</h1>
+class LoginPage extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Novi's competitiemanager</h1>
 
-            <form action="">
-                <input type="text" placeholder="gebruikersnaam" />
-                <br/>
-                <input type="password" placeholder="wachtwoord"/>
-                <br/>
-                <button>Login</button>
-                <p>een paar lieve woordjes.</p>
-            </form>
-        </div>
-    );
+                <form action="">
+                    <input type="text" placeholder="gebruikersnaam"/>
+                    <br/>
+                    <input type="password" placeholder="wachtwoord"/>
+                    <br/>
+                    <button>Login</button>
+                    <p>een paar lieve woordjes.</p>
+                </form>
+            </div>
+        );
+    }
 }
 
 export default LoginPage;
