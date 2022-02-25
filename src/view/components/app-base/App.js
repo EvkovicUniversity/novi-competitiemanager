89cb5fdb479112d0c5bd43e286a736fc701c75d1
unknown <e.olijrhook@noviuniversity.com>
2022-02-25 12:18:38 +0100

    styling van de webapp basis

--- src/view/components/app-base/App.js
+++ src/view/components/app-base/App.js
@@ -1,27 +1,26 @@
-import logo from '../../resources/logo/Novi_Logo_Schematisch.png';
+import logo from '../../resources/logo/novi_logo.PNG';
 import './App.css';
 import '../algemeen/algemeen.css';
 
 function App() {
-  return (
-    <div className="background">
+    return (
+        <div className="background">
+            <div className="outer_content_container">
+                <div className="inner_content_container">
+                    <img src={logo} className="novi-logo" alt="novi-logo"/>
+                    <div className="alternating_content_container">
 
-        <div className="content_container">
+                        <h1>Novi Competitiemanager</h1>
 
-            <img src={logo} className="novi-logo" alt="novi-logo" />
+                        <p>
+                            <button className="button01" id="testbutton">KNOP</button>
+                        </p>
+                    </div>
 
-            <div className="alternating_content">
-                <p>
-                    Ik verander wel eens.
-                </p>
+                </div>
             </div>
-
-            <p>
-                <button className="button01" id="testbutton">KNOP</button>
-            </p>
         </div>
-    </div>
-  );
+    );
 }
 
 export default App;
