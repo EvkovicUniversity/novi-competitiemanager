626e4f7892db50b00d2e9389e8547c12edcebd2c
unknown <e.olijrhook@noviuniversity.com>
2022-02-25 12:30:49 +0100

    eerste opzet loging page

--- src/view/components/app-base/App.js
+++ src/view/components/app-base/App.js
@@ -1,6 +1,7 @@
 import logo from '../../resources/logo/novi_logo.PNG';
 import './App.css';
 import '../algemeen/algemeen.css';
+import LoginPage from "../login-page/login-page";
 
 function App() {
     return (
@@ -9,14 +10,8 @@ function App() {
                 <div className="inner_content_container">
                     <img src={logo} className="novi-logo" alt="novi-logo"/>
                     <div className="alternating_content_container">
-
-                        <h1>Novi Competitiemanager</h1>
-
-                        <p>
-                            <button className="button01" id="testbutton">KNOP</button>
-                        </p>
+                        <LoginPage/>
                     </div>
-
                 </div>
             </div>
         </div>
