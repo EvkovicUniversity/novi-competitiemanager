f5647bee94e82e88b8adf159ad8859e1b04f7f5a
Evan Olijrhook <e.olijrhook@novi-education.nl>
2022-03-02 11:22:41 +0100

    nu met goeie spring backend, I hope...

---
+++ back end/src/main/java/com/example/novi/novicompetitiemanagerbackend/NoviCompetitiemanagerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.novi.novicompetitiemanagerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoviCompetitiemanagerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoviCompetitiemanagerBackendApplication.class, args);
+	}
+
+}
---
+++ back end/src/test/java/com/example/novi/novicompetitiemanagerbackend/NoviCompetitiemanagerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.novi.novicompetitiemanagerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoviCompetitiemanagerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
